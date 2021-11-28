0d2cbe8cc6ca42f93d7a88a187e801eaa8221568
Dominik Stadler <dominik.stadler@gmx.at>
2021-11-28 16:55:28 +0100

    Add very simple unit-test
    This is not the actual fuzzing!

---
+++ src/test/java/org/dstadler/poi/fuzz/FuzzTest.java
@@ -0,0 +1,12 @@
+package org.dstadler.poi.fuzz;
+
+import org.junit.jupiter.api.Test;
+
+class FuzzTest {
+	@Test
+	public void test() {
+		Fuzz.fuzzerTestOneInput(new byte[] {});
+		Fuzz.fuzzerTestOneInput(new byte[] {1});
+		Fuzz.fuzzerTestOneInput(new byte[] {'P', 'K'});
+	}
+}
