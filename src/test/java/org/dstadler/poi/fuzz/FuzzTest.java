3caaef75c808136ae6c01545e4be76576b5c1ed8
Dominik Stadler <dominik.stadler@gmx.at>
2021-11-28 19:07:14 +0100

    Show how a slow-unit result can be re-run

--- src/test/java/org/dstadler/poi/fuzz/FuzzTest.java
+++ src/test/java/org/dstadler/poi/fuzz/FuzzTest.java
@@ -1,7 +1,12 @@
 package org.dstadler.poi.fuzz;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class FuzzTest {
@@ -17,5 +22,11 @@ class FuzzTest {
 		// should not be logged
 		Logger LOG = LogManager.getLogger(FuzzTest.class);
 		LOG.atError().log("Test log output which should not be visible -----------------------");
+	}
+
+	@Disabled("Local test for verifying a slow run")
+	@Test
+	public void testSlowUnit() throws IOException {
+		Fuzz.fuzzerTestOneInput(FileUtils.readFileToByteArray(new File("slow-unit-0a0b0ce97bb332cd9f8fde03e03840768a81d29d")));
 	}
 }
