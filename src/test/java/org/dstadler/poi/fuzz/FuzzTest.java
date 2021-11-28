f9b2e17f9f6a44f1added2a071441da6e790c099
Dominik Stadler <dominik.stadler@gmx.at>
2021-11-28 18:38:32 +0100

    Add custom log4j-configuration to not log out any errors while fuzzing

--- src/test/java/org/dstadler/poi/fuzz/FuzzTest.java
+++ src/test/java/org/dstadler/poi/fuzz/FuzzTest.java
@@ -1,5 +1,7 @@
 package org.dstadler.poi.fuzz;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 
 class FuzzTest {
@@ -8,5 +10,18 @@ class FuzzTest {
 		Fuzz.fuzzerTestOneInput(new byte[] {});
 		Fuzz.fuzzerTestOneInput(new byte[] {1});
 		Fuzz.fuzzerTestOneInput(new byte[] {'P', 'K'});
+	}
+
+	@Test
+	public void testLog() {
+		// trigger loading the class and thus call the static code
+		Fuzz.fuzzerTestOneInput(new byte[] {});
+
+		// need to create the logger after initializing
+		Logger LOG = LogManager.getLogger(FuzzTest.class);
+
+		// should not be logged
+		LOG.atError().log("Test log output which should not be visible -----------------------");
+
 	}
 }
