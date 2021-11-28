bf83426bd20e8a2d07856612186700f430635eea
Dominik Stadler <dominik.stadler@gmx.at>
2021-11-28 18:10:46 +0100

    Add some description, add more exceptions, start to add some TODOs

--- src/main/java/org/dstadler/poi/fuzz/Fuzz.java
+++ src/main/java/org/dstadler/poi/fuzz/Fuzz.java
@@ -13,9 +13,11 @@ import org.apache.poi.hslf.usermodel.HSLFSlideShowImpl;
 import org.apache.poi.hssf.OldExcelFormatException;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.ooxml.POIXMLException;
 import org.apache.poi.openxml4j.exceptions.NotOfficeXmlFileException;
 import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.poifs.filesystem.OfficeXmlFileException;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
@@ -27,13 +29,22 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.xmlbeans.XmlException;
 
+/**
+ * This class provides a target for fuzzing Apache POI with Jazzer.
+ *
+ * It catches all exceptions that are currently expected.
+ *
+ * Some are marked as to-do where Apache POI should actually do
+ * exception handling differently, e.g. wrapping internal exceptions
+ * or providing more explicit exceptions instead of general RuntimeExceptions
+ */
 public class Fuzz {
 	@SuppressWarnings("EmptyTryBlock")
 	public static void fuzzerTestOneInput(byte[] input) {
 		// try to invoke various methods which parse documents/workbooks/slide-shows/...
 
 		try (Workbook ignored = WorkbookFactory.create(new ByteArrayInputStream(input))) {
-		} catch (IOException | EncryptedDocumentException | EmptyFileException | NotOfficeXmlFileException e) {
+		} catch (IOException | EncryptedDocumentException | EmptyFileException | NotOfficeXmlFileException | POIXMLException | OfficeXmlFileException e) {
 			// expected here
 		}
 
@@ -48,56 +59,58 @@ public class Fuzz {
 
 			try (HPSFPropertiesOnlyDocument ignored = new HPSFPropertiesOnlyDocument(fs)) {
 			}
-		} catch (IOException | OldExcelFormatException e) {
+		} catch (IOException | OldExcelFormatException | OfficeXmlFileException e) {
 			// expected here
 		}
 
 		try (HSSFWorkbook ignored = new HSSFWorkbook(new ByteArrayInputStream(input))) {
-		} catch (IOException e) {
+		} catch (IOException | OfficeXmlFileException e) {
 			// expected here
 		}
 
 		try (HSLFSlideShow ignored = new HSLFSlideShow(new ByteArrayInputStream(input))) {
-		} catch (IOException e) {
+		} catch (IOException | OfficeXmlFileException e) {
 			// expected here
 		}
 
 		try (HSLFSlideShowImpl ignored = new HSLFSlideShowImpl(new ByteArrayInputStream(input))) {
-		} catch (IOException e) {
+		} catch (IOException | OfficeXmlFileException e) {
 			// expected here
 		}
 
 		try (HWPFDocument ignored = new HWPFDocument(new ByteArrayInputStream(input))) {
-		} catch (IOException | /*EmptyFileException | */ IllegalArgumentException e) {
+		} catch (IOException | /*EmptyFileException | */
+				// TODO: replace with a more specific exception
+				IllegalArgumentException e) {
 			// expected here
 		}
 
 		try (XSSFWorkbook wb = new XSSFWorkbook(new ByteArrayInputStream(input))) {
 			try (SXSSFWorkbook ignored = new SXSSFWorkbook(wb)) {
 			}
-		} catch (IOException | EmptyFileException | NotOfficeXmlFileException e) {
+		} catch (IOException | EmptyFileException | NotOfficeXmlFileException | POIXMLException e) {
 			// expected here
 		}
 
 		try (XWPFDocument ignored = new XWPFDocument(new ByteArrayInputStream(input))) {
-		} catch (IOException | EmptyFileException | NotOfficeXmlFileException e) {
+		} catch (IOException | EmptyFileException | NotOfficeXmlFileException | POIXMLException e) {
 			// expected here
 		}
 
 		try (XMLSlideShow ignored = new XMLSlideShow(new ByteArrayInputStream(input))) {
-		} catch (IOException | EmptyFileException | NotOfficeXmlFileException e) {
+		} catch (IOException | EmptyFileException | NotOfficeXmlFileException | POIXMLException e) {
 			// expected here
 		}
 
 		try (OPCPackage pkg = OPCPackage.open(new ByteArrayInputStream(input))) {
 			try (XSLFSlideShow ignored = new XSLFSlideShow(pkg)) {
 			}
-		} catch (IOException | OpenXML4JException | XmlException | EmptyFileException | NotOfficeXmlFileException e) {
+		} catch (IOException | OpenXML4JException | XmlException | EmptyFileException | NotOfficeXmlFileException | POIXMLException e) {
 			// expected here
 		}
 
 		try (XmlVisioDocument ignored = new XmlVisioDocument(new ByteArrayInputStream(input))) {
-		} catch (IOException | EmptyFileException | NotOfficeXmlFileException e) {
+		} catch (IOException | EmptyFileException | NotOfficeXmlFileException | POIXMLException e) {
 			// expected here
 		}
 	}
