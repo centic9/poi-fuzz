4d5adf7dc897dfbd44a09a9dffd67f47cec31dc9
Dominik Stadler <dominik.stadler@gmx.at>
2022-02-11 08:43:29 +0100

    Adjust instrumentation in coverage-report

--- coverageReport.sh
+++ coverageReport.sh
@@ -43,7 +43,7 @@ mkdir -p build/jacoco
 # Run Jazzer with JaCoCo-Agent to produce coverage information
 ./jazzer \
   --cp=build/libs/poi-fuzz-all.jar \
-  --instrumentation_includes=org.apache.poi.** \
+  --instrumentation_includes=org.apache.poi.**:org.apache.xmlbeans.** \
   --target_class=org.dstadler.poi.fuzz.Fuzz \
   --nohooks \
   --jvm_args="-XX\\:-OmitStackTraceInFastThrow:-javaagent\\:build/jacocoagent.jar=destfile=build/jacoco/corpus.exec" \
