7dc82c174407b40f78a7c213ff139dae7babd8f7
Dominik Stadler <dominik.stadler@gmx.at>
2023-01-16 14:09:21 +0100

    Expect more exceptions in XLSX2CSV

--- src/main/java/org/dstadler/poi/fuzz/FuzzXLSX2CSV.java
+++ src/main/java/org/dstadler/poi/fuzz/FuzzXLSX2CSV.java
@@ -5,11 +5,11 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.commons.io.output.NullPrintStream;
-import org.apache.poi.EmptyFileException;
 import org.apache.poi.examples.xssf.eventusermodel.XLSX2CSV;
-import org.apache.poi.openxml4j.exceptions.NotOfficeXmlFileException;
+import org.apache.poi.ooxml.POIXMLException;
 import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.util.RecordFormatException;
 import org.xml.sax.SAXException;
 
 public class FuzzXLSX2CSV {
@@ -19,8 +19,9 @@ public class FuzzXLSX2CSV {
 			OPCPackage p = OPCPackage.open(in);
 			XLSX2CSV xlsx2csv = new XLSX2CSV(p, new NullPrintStream(), 5);
 			xlsx2csv.process();
-		} catch (IOException | OpenXML4JException | SAXException | EmptyFileException |
-				 NotOfficeXmlFileException e) {
+		} catch (IOException | OpenXML4JException | SAXException |
+				 POIXMLException | RecordFormatException |
+				IllegalStateException | IllegalArgumentException e) {
 			// expected here
 		}
 	}
