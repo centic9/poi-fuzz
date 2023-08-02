ce0895cb9084ad8a13b2229e86c3cae13e256505
Dominik Stadler <dominik.stadler@gmx.at>
2023-08-02 16:41:42 +0200

    Adjust computing coverage and use the same class-files

--- coverageReport.sh
+++ coverageReport.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 #
 # Small helper script to produce a coverage report when executing the fuzz-model
@@ -55,12 +55,17 @@ for i in `cd src/main/java/org/dstadler/poi/fuzz/ && ls Fuzz${ONE}*.java`; do
   CLASS=`echo $i | sed -e 's/.java//g'`
   CORPUS=`echo ${CLASS} | sed -e 's/Fuzz/corpus/g'`
 
+  # no separate corpus for this one
+  if [ "${CLASS}" == FuzzXLSX2CSV ]; then
+    continue
+  fi
+
   echo
   echo Running jazzer for ${CLASS} and ${CORPUS}
 
   # Run Jazzer with JaCoCo-Agent to produce coverage information
   ./jazzer \
-    --cp=/opt/poi/build/dist/maven/poi/poi-5.2.4-SNAPSHOT.jar:/opt/poi/build/dist/maven/poi-ooxml/poi-ooxml-5.2.4-SNAPSHOT.jar:/opt/poi/build/dist/maven/poi-scratchpad/poi-scratchpad-5.2.4-SNAPSHOT.jar:build/libs/poi-fuzz-all.jar \
+    --cp=build/poifiles:build/libs/poi-fuzz-all.jar \
     --instrumentation_includes=org.apache.poi.**:org.apache.xmlbeans.** \
     --target_class=org.dstadler.poi.fuzz.${CLASS} \
     --nohooks \
