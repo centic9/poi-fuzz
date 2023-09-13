f7bfbc7c0a1c41761e8c0f6e523d0e07318d0507
Dominik Stadler <dominik.stadler@dynatrace.com>
2023-09-13 16:15:00 +0200

    FuzzHWPF: One more expected exception

--- src/main/java/org/dstadler/poi/fuzz/FuzzHWPF.java
+++ src/main/java/org/dstadler/poi/fuzz/FuzzHWPF.java
@@ -6,6 +6,7 @@ import java.nio.BufferUnderflowException;
 import java.util.NoSuchElementException;
 
 import org.apache.commons.io.output.NullOutputStream;
+import org.apache.poi.hssf.record.RecordInputStream;
 import org.apache.poi.hwpf.HWPFDocument;
 import org.apache.poi.hwpf.extractor.WordExtractor;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
@@ -29,7 +30,8 @@ public class FuzzHWPF {
 			}
 		} catch (IOException | IllegalArgumentException | IndexOutOfBoundsException | BufferUnderflowException |
 				NoSuchElementException | RecordFormatException | IllegalStateException |
-				DocumentFormatException | UnsupportedOperationException | NegativeArraySizeException e) {
+				DocumentFormatException | UnsupportedOperationException | NegativeArraySizeException |
+				 RecordInputStream.LeftoverDataException e) {
 			// expected here
 		}
 	}
